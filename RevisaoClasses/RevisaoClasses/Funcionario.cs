9f94c0d11b2ef23b0dafa65911ec1aac62a0bcb3
Gustavo Souza <gustavo.souza.1449179@sga.pucminas.br>
2023-08-09 19:28:08 -0300

    revisao de classes

---
+++ RevisaoClasses/RevisaoClasses/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Funcionarios
+{
+    public string nome;
+    public double salario;
+
+    public Funcionarios(string nome, double salario)
+    {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+}
--- RevisaoClasses/RevisaoClasses/Program.cs
+++ RevisaoClasses/RevisaoClasses/Program.cs
@@ -6,6 +6,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Teste")
+        List<Funcionarios> listaFuncionarios = new List<Funcionarios>();
+
+
+
+        for (int i = 0; i < 2; i++)
+        {
+            Console.WriteLine($"Digite o nome do {i + 1}funcinario");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine($"Digite o salario do {i + 1} funcionario ");
+            double salario = double.Parse(Console.ReadLine());
+
+            Funcionarios funcionario = new Funcionarios (nome,salario);
+
+            listaFuncionarios.Add(funcionario);           
+
+        }
+
+        double mediasalarios = Media(listaFuncionarios[0].salario, listaFuncionarios[1].salario);
+
+        Console.WriteLine(mediasalarios);
+    }
+
+    static double Media(double a , double b) {
+        return (a + b)/ 2 ;
+    
     }
 }
